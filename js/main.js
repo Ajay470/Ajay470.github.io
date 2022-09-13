5ed86bd8e97a36c90a01f0b8dd79001d3213b12b
Ajay470 <105915458+Ajay470@users.noreply.github.com>
2022-09-13 09:17:26 +0530

    change plan

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Navigation bar effects on scroll
+
+//Portfolio section - Modal
+
+//Our clients - Swiper
+
+//Website dark/light theme
+
+//Scroll to top button
+
+//Navigation menu items active on page scroll
+
+//Responsive navigation menu toggle
+
+//Scroll reveal animations
+//Common reveal options to create reveal animations
+
+//Target elements, and specify options to create reveal animations
